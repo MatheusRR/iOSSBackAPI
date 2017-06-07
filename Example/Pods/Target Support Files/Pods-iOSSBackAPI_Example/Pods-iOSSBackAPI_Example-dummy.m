b43d6e5ecfe3c4e254865b084549dec7090b932c
Matheus Ribeiro <empresas.mr.two@gmail.com>
2017-06-07 19:26:40 -0300

    Create iOS ShopBack API via pod crete lib.

---
+++ Example/Pods/Target Support Files/Pods-iOSSBackAPI_Example/Pods-iOSSBackAPI_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSSBackAPI_Example : NSObject
+@end
+@implementation PodsDummy_Pods_iOSSBackAPI_Example
+@end
---
+++ Example/Pods/Target Support Files/iOSSBackAPI/iOSSBackAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSSBackAPI : NSObject
+@end
+@implementation PodsDummy_iOSSBackAPI
+@end
